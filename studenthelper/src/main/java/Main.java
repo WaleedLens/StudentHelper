2cbc0740d92483f354b0ffede4288d4628072cce
Waleed Qoozi <90866317+WaleedLens@users.noreply.github.com>
2023-04-15 22:12:51 +0300

    created routing & Initiated tomcat

--- studenthelper/src/main/java/Main.java
+++ studenthelper/src/main/java/Main.java
@@ -1,7 +1,15 @@
+import core.Webserver;
+import requests.DemoRequest;
+import requests.HttpRequest;
+
 public class Main {
 
 
     public static void main(String[] args){
+        Webserver wb = new Webserver();
+        wb.route(DemoRequest.class,"DemoRequest","/demo");
+        wb.route(HttpRequest.class,"HttpRequest","/req");
 
+        wb.startServer();
     }
 }
--- studenthelper/src/main/java/core/Webserver.java
+++ studenthelper/src/main/java/core/Webserver.java
@@ -1,11 +1,57 @@
 package core;
 
 import jakarta.servlet.http.HttpServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+import requests.HttpRequest;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class Webserver {
+    private  Wrapper servletWrapper;
+    private Context context;
+    private Tomcat tomcat;
 
+    public Webserver(){
+        tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+        context = tomcat.addContext("",null);
 
-    public void init(){
+    }
 
+    /**
+     * Start tomcat server
+     */
+    public void startServer(){
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Construct Route
+     * @param servlet A class extends HttpServlet
+     * @param servletName Servlet name
+     * @param route route path (e.g., /path)
+     */
+    public void route(Class<? extends HttpServlet> servlet,String servletName,String route){
+        try {
+           servletWrapper = tomcat.addServlet(context,servletName,servlet.getDeclaredConstructor().newInstance());
+           servletWrapper.addMapping(route);
+           servletWrapper.setLoadOnStartup(1);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
